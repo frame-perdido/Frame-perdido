142fa86e5defe34d39d1469c8011c63ed0dbda31
frame-perdido <jonasfed96@gmail.com>
2026-09-07 11:50:05 -0600

    Update data.js

--- data.js
+++ data.js
@@ -2682,3 +2682,9 @@ var animes = [{
     hentai: false,
     ecchi: false
 }];
+// ============================================================
+// COMBINAR CON data-02.js (obras nuevas 107+)
+// ============================================================
+if (typeof animes02 !== 'undefined') {
+    animes = animes.concat(animes02);
+}
