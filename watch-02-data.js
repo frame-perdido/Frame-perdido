2b7fcb00e3fc4f1798a29552b2526a8603587271
frame-perdido <jonasfed96@gmail.com>
2026-09-08 10:47:33 -0600

    Create watch-02-data.js

---
+++ watch-02-data.js
@@ -0,0 +1,24 @@
+// watch-02-data.js - Base de datos para watch.html (107+)
+const videos2 = {
+    107: {
+    title: "Lucy, la hija del diablo",
+    year: "2005-2007",
+    studio: "Fluid Animation",
+    director: "Loren Bouchard",
+    desc: "Lucy es una chica de 21 años que vive en San Francisco y resulta ser la hija de Satanás. Su padre la ha elegido para que se convierta en el Anticristo, aunque a ella no le hace ninguna gracia y prefiere seguir con su vida normal, estudiando arte y saliendo con su novio, DJ Jesús.",
+    tags: ["Comedia", "Comedia negra", "Animación para adultos", "Culto"],
+    episodios: [
+        { num: 1, version: "Dob", url: "//ok.ru/videoembed/15699986877071?nochat=1" },
+        { num: 2, version: "Dob", url: "//ok.ru/videoembed/15699986549391?nochat=1" },
+        { num: 3, version: "Dob", url: "//ok.ru/videoembed/15699986680463?nochat=1" },
+        { num: 4, version: "Dob", url: "//ok.ru/videoembed/15699986483855?nochat=1" },
+        { num: 5, version: "Dob", url: "//ok.ru/videoembed/15699986745999?nochat=1" },
+        { num: 6, version: "Dob", url: "//ok.ru/videoembed/15699986942607?nochat=1" },
+        { num: 7, version: "Dob", url: "//ok.ru/videoembed/15699987401359?nochat=1" },
+        { num: 8, version: "Dob", url: "//ok.ru/videoembed/15699987008143?nochat=1" },
+        { num: 9, version: "Dob", url: "//ok.ru/videoembed/15699987335823?nochat=1" },
+        { num: 10, version: "Dob", url: "//ok.ru/videoembed/15699987270287?nochat=1" },
+        { num: 11, version: "Dob", url: "//ok.ru/videoembed/15699987139215?nochat=1" }
+    ]
+}
+};
