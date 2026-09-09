f36c8aa6c7625a39492d50f0887afeba33fb3712
frame-perdido <jonasfed96@gmail.com>
2026-09-09 12:52:40 -0600

    Update watch-02-data.js

--- watch-02-data.js
+++ watch-02-data.js
@@ -20,5 +20,29 @@ const videos2 = {
         { num: 10, version: "Dob", url: "//ok.ru/videoembed/15699987270287?nochat=1" },
         { num: 11, version: "Dob", url: "//ok.ru/videoembed/15699987139215?nochat=1" }
     ]
+},
+    108: {
+    title: "Fritz el Gato",
+    year: 1972,
+    studio: "Bakshi Productions",
+    director: "Ralph Bakshi",
+    desc: "Fritz es un gato universitario y mujeriego que busca sexo, drogas y experiencias en un viaje caótico a través de la contracultura de los 70. Una sátira social mordaz y la primera película animada en recibir una calificación X.",
+    tags: ["Comedia", "Sátira", "Animación para adultos", "Contracultura", "Culto"],
+    episodios: [
+        { num: 1, version: "Dob", url: "//ok.ru/videoembed/4296221002309?nochat=1" },
+        { num: 1, version: "Sub", url: "//ok.ru/videoembed/2221314476667?nochat=1" }
+    ]
+},
+    109: {
+    title: "Las nueve vidas de Fritz el Gato",
+    year: 1974,
+    studio: "Krantz Films / Cinemation Industries",
+    director: "Robert Taylor",
+    desc: "Secuela de Fritz el Gato realizada sin la participación de Ralph Bakshi ni de Robert Crumb. Un formato de antología que explora nueve vidas alternativas del gato más famoso de la animación adulta. La primera película animada en recibir una calificación R.",
+    tags: ["Comedia", "Sátira", "Animación para adultos", "Antología", "Culto"],
+    episodios: [
+         { num: 1, version: "Sub", url: "//ok.ru/videoembed/4296221329989?nochat=1" },
+          { num: 1, version: "Sub 1080p", url: "//ok.ru/videoembed/8064839780950?nochat=1" }
+    ]
 }
 };
