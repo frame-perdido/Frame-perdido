fbec8423363b4175a0398fcbb503f8acca653c4b
frame-perdido <jonasfed96@gmail.com>
2026-09-17 13:44:04 -0600

    Create series-rotas.js

---
+++ series-rotas.js
@@ -0,0 +1,45 @@
+// ============================================================
+// series-rotas.js
+// Series con video roto, bloqueado o de fuente inestable
+// ============================================================
+
+window.SERIES_ROTAS = [
+  
+    "dragon-quest-las-aventuras-de-fly",
+    "voltron",
+    "battle-commander-daimos",
+    "macros-zero",
+    "bionic-six",
+    "las-nuevas-aventuras-de-he-man",
+    "ren-y-stimpy",
+    "la-princesa-de-los-mil-an-os",
+    "dragon-ball-super",
+    "dragon-ball",
+    "dragon-ball-z",
+    "saint-seiya-omega",
+    "saint-seiya-soul-of-gold",
+    "saintia-sho",
+    "hunter-x-hunter",
+    "berserk",
+    "zoids",
+    "yu-yu-hakusho",
+    "utena",
+    "ojos-de-gato",
+    "espi-ritu-de-lucha",
+    "power-rangers-dino-trueno",
+
+    
+    "slam-dunk",
+    "sailor-moon",
+    "kurochan",
+    "sailor-moon-crystal",
+
+  
+    "los-centuriones",
+    "capitan-futuro",
+    "super-agente-cobra",
+    "marco",
+    "la-abeja-maya",
+    "remi-el-ni-o-de-nadie",
+    "las-guerreras-m-gicas",
+];
