cd7baa0084edda82299fcd0429286866c523eb08
frame-perdido <jonasfed96@gmail.com>
2026-09-16 20:01:44 -0600

    Create portadas-fix.js

---
+++ portadas-fix.js
@@ -0,0 +1,12 @@
+// ============================================================
+// portadas-fix.js
+// ============================================================
+
+window.PORTADAS_FIX = {
+
+    "power-ranger-mighty-morphin": "https://blogger.googleusercontent.com/img/a/AVvXsEhky4Pe2spyiNIykuCWWeAg9oCDrg0YMI4IbYrJpM1YZjT6ujaUa79GYl9fg-shx9FYBhthovmCNko0FtCa06cK6TNaPNvubW_fns_FO2_96lZ4-SmS_QpJUpXE9EDzDdn7RqUITxwGomm80N5PYQMqT0D021GURT1SOhRuyhjO8IDWRZp5D_cW6voMiP3w=w285-h400",
+    "power-rangers-rpm": "https://blogger.googleusercontent.com/img/a/AVvXsEguUfFzLNlKXQtrTnRo2OUTmyBQ1XxfAs4wIwqHcYCoauzaLPhk7km3TeaXEGvcc5zb0p3McY-S3ayCSck32Tt6oX3Ht0PYMeUtfnZcCIL6dmCCvXd78muZmUY5sF_GZjee4KGqRW5Nr_FKfJShnf8qcWDPyUBksPqh3mJ9R-b0zrcRwh0aEpkvpqBXNkvi=w307-h400",
+    "power-rangers-super-ninja-steel": "https://blogger.googleusercontent.com/img/a/AVvXsEhfjyg6yqJNzDXiMO3qXOcO77-tU_vb46AOdAO6wJodD-RCFGlmE_WSvYBO29aS4_bwgtQ0rcuSS8v2vc0slQ_G9cGYi1nQLgF0V1HXZHFtQU-lkU5ALIx0zSWaC9AZNp2qkCJLtWA7GlzvZenF9ZKslPaGkFJnnjRxOCwvvWseclnZBZ2Tjq8PF3uFBPRF=w320-h400",
+    "power-rangers-time-force": "https://blogger.googleusercontent.com/img/a/AVvXsEgPko8x-ttxGNo7fbUp-xdhsnlI69GTVv2FoVEO6Km7NrbuKAWnSiK1UqjPPn7jN_D_oDItD67YZYZsq3vjaKx9UlLqLhuxB1NjBSWLzFpkCdSIItiRwKVn5liu26lzH0IEtNCEA-qcMhIy9aqyl2xiaALz0_H6IoSxliv1rkMwhBO9KgX23wF8UcIq9jzH=w258-h400",
+
+};
